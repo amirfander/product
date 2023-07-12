625faf23498a14da35bce888c1d5c49892156b26
amirmf <amirfander@gmail.com>
2023-07-12 14:31:08 +0330

    add subscribe

--- config/broker/nats/repository.go
+++ config/broker/nats/repository.go
@@ -1,6 +1,7 @@
 package nats
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/nats-io/nats.go"
@@ -19,4 +20,12 @@ func (n Nats) Publish(subject string, data []byte) {
 		fmt.Println(err)
 	}
 	fmt.Println("publish message")
+}
+
+func (n Nats) Subscribe(subject string, callback func(data interface{})) {
+	connection.Subscribe(subject, func(msg *nats.Msg) {
+		var data interface{}
+		json.Unmarshal(msg.Data, &data)
+		callback(data)
+	})
 }
